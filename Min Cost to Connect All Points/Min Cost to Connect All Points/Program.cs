59d520a6c4fd940d29d7ff5a0f6e9474976f8bf8
Jon-Dev21 <jonacevedev96@gmail.com>
2022-04-26 00:55:45 -0400

    Added Problem #1584. Min Cost to Connect All Points
    You are given an array points representing integer coordinates of some points on a 2D-plane, where points[i] = [xi, yi].
    
    The cost of connecting two points [xi, yi] and [xj, yj] is the manhattan distance between them: |xi - xj| + |yi - yj|, where |val| denotes the absolute value of val.
    
    Return the minimum cost to make all points connected. All points are connected if there is exactly one simple path between any two points.

---
+++ Min Cost to Connect All Points/Min Cost to Connect All Points/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Min_Cost_to_Connect_All_Points // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter how many points you want to enter: ");
+            string pointCnt = Console.ReadLine();
+            int[][] points = new int[][] {
+                new int[] {0, 0},
+                new int[] {2, 2},
+                new int[] {3, 10},
+                new int[] {5, 2},
+                new int[] {7, 0}
+            };
+        }
+
+
+        public static int MinCostConnectPoints(int[][] points)
+        {
+            int minCost = 0;
+            return minCost;
+        }
+    }
+}
