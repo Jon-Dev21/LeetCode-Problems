0595d45163971f6bab5a85580a2134906753b634
Jon-Dev21 <jonacevedev96@gmail.com>
2022-04-26 18:11:45 -0400

    Formatting changes / Finished Version

--- Contains-Duplicate/Contains-Duplicate/Program.cs
+++ Contains-Duplicate/Contains-Duplicate/Program.cs
@@ -3,12 +3,13 @@
     public bool ContainsDuplicate(int[] nums)
     {
         List<int> unique = new List<int>(nums.Length);
-
-
         foreach (int num in nums)
         {
+            if(unique.Contains(num))
+                return true;
             unique.Add(num);
-
+            
         }
+        return false;
     }
 }
