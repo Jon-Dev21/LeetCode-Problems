fa813e025b339ef098f86fea12f657fcc5238069
Jon-Dev21 <jonacevedev96@gmail.com>
2022-04-27 00:13:31 -0400

    Great solution found
    Runtime: 137 ms, faster than 17.29% of C# online submissions for Valid Anagram.
    
    Memory Usage: 39.6 MB, less than 35.89% of C# online submissions for Valid Anagram.
    
    Way faster than other tries.
    
    2686 ms faster and saved 2.9 MB
    
    Props to Neetcode for the explanation. Tried hard but didn't figure it out by myself. Will learn from this though.

--- Valid Anagram/Valid Anagram/Program.cs
+++ Valid Anagram/Valid Anagram/Program.cs
@@ -2,13 +2,65 @@
 {
     static void Main(string[] args)
     {
+        var watch = new System.Diagnostics.Stopwatch();
+
+        watch.Start();
         Console.WriteLine(IsAnagram("aacc", "ccac"));
-        Console.WriteLine(IsAnagram("a", "ab"));
-        Console.WriteLine(IsAnagram("rat", "car"));
-        Console.WriteLine(IsAnagram("rat", "tar"));
-        Console.WriteLine(IsAnagram("anagram", "margana"));
-        Console.WriteLine(IsAnagram("anagram", "nagaram"));
+        //Console.WriteLine(IsAnagram("a", "ab"));
+        //Console.WriteLine(IsAnagram("rat", "car"));
+        //Console.WriteLine(IsAnagram("rat", "tar"));
+        //Console.WriteLine(IsAnagram("anagram", "margana"));
+        //Console.WriteLine(IsAnagram("anagram", "nagaram"));
+
+        watch.Stop();
+
+        Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
     }
+
+    public static bool IsAnagram(string s, string t)
+    {
+        var a = s.ToArray();
+        var b = t.ToArray();
+        Array.Sort(a);
+        Array.Sort(b);
+        return new string(a) == new string(b);
+    }
+
+    /***
+     * This solution runs at 3ms with this test.
+     * Console.WriteLine(IsAnagram("aacc", "ccac"));
+     * 
+     * 
+    public static bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length)
+        {
+            return false;
+        }
+        else
+        {
+            int equalLetters = 0;
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (s.IndexOf(t[i]) == -1)
+                    return false;
+                else
+                {
+                    int pos = s.IndexOf(t[i]);
+                    equalLetters++;
+                    s = s.Substring(0, pos) + s.Substring(pos + 1);
+                }
+
+            }
+            return (equalLetters == t.Length ? true : false);
+        }
+    }*/
+
+    /**
+     * This method works but it's too slow.
+     * Was not submitted due to too much time. 7ms
+     * Console.WriteLine(IsAnagram("aacc", "ccac"));
+     * 
 
     public static bool IsAnagram(string s, string t)
     {
@@ -18,23 +70,30 @@
         } 
         else
         {
-            int equalLetters = 0;
+            sbyte cnt;
+            Dictionary<char, sbyte> HashMap_S = new Dictionary<char, sbyte>();
+            sbyte cnt2;
+            Dictionary<char, sbyte> HashMap_T = new Dictionary<char, sbyte>();
             for (int i = 0; i < t.Length; i++)
             {
-                if(s.IndexOf(t[i]) == -1)
+                cnt = (sbyte)s.Count(f => (f == s[i]));
+                if(cnt == 0)
                     return false;
-                else
-                {
-                    int pos = s.IndexOf(t[i]);
-                    equalLetters++;
-                    s = s.Substring(0, pos) +s.Substring(pos+1);
-                }
-                    
+                else if(!HashMap_S.ContainsKey(s[i]))
+                    HashMap_S.Add(s[i], cnt);
+
+                cnt2 = (sbyte)t.Count(f => (f == s[i]));
+                if (cnt == 0)
+                    return false;
+                else if (!HashMap_T.ContainsKey(s[i]))
+                    HashMap_T.Add(s[i], cnt2);
+
+                if(HashMap_S[s[i]] != HashMap_T[s[i]])
+                    return false;
             }
-            return (equalLetters == t.Length ? true : false);
+            return true;
         }
-            
-    }
+    }*/
 }
 
 
