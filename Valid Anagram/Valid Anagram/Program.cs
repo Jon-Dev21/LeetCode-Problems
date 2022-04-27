7d098b7848d44da41278fd3dc643d6ee4bb5a27c
Jon-Dev21 <jonacevedev96@gmail.com>
2022-04-26 21:29:45 -0400

    Adding Problem 242. Valid Anagram
    Problem Description: An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
    
    Currently this version of the program does not work for all test cases. Particularly complaining on "aacc"
    "ccac". Will investigate, fix and submit.

---
+++ Valid Anagram/Valid Anagram/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(IsAnagram("a", "ab"));
+        Console.WriteLine(IsAnagram("rat", "car"));
+        Console.WriteLine(IsAnagram("rat", "tar"));
+        Console.WriteLine(IsAnagram("anagram", "margana"));
+        Console.WriteLine(IsAnagram("anagram", "nagaram"));
+    }
+
+    public static bool IsAnagram(string s, string t)
+    {
+        if(s.Length != t.Length)
+        {
+            return false;
+        } 
+        else
+        {
+            int equalLetters = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j < s.Length; j++)
+                {
+                    if (s[i] == t[j])
+                    {
+                        equalLetters++;
+                        break;
+                    }
+                }
+            }
+            return (equalLetters == s.Length ? true : false);
+        }
+            
+    }
+}
+
+
