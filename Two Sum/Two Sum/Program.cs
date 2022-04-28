e9becba9ae777437410ebb2f007b4e60c0700338
Jon-Dev21 <jonacevedev96@gmail.com>
2022-04-27 22:41:34 -0400

    Adding Problem Two Sum (Approved)
    Runtime: 233 ms, faster than 37.19% of C# online submissions for Two Sum.
    
    Memory Usage: 42.7 MB, less than 84.08% of C# online submissions for Two Sum.
    
    I believe I can also remove the first if statement and make it even better.

---
+++ Two Sum/Two Sum/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    static void main(string[] args)
+    {
+        Console.WriteLine(TwoSum(new int[] {3,2,3}, 6));
+    }
+
+    public static int[] TwoSum(int[] nums, int target)
+    {
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for(int j = i+1; j < nums.Length; j++)
+            {
+                if(j < nums.Length)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j};
+                    }  
+                }
+            }
+
+
+            //if(i+1 < nums.Length) // Avoid out of bounds.
+            //{
+            //    if(nums[i] + nums[i+1] == target)
+            //    {
+            //        return new int[] { i, i + 1};
+            //    }
+            //} else if (nums[i] + nums[nums.Length-1] == target)
+            //{
+            //    return new int[] { i, nums.Length - 1 };
+            //}
+        }
+        return new int[] {0,0};
+    }
+}
