11ecf011531a1385b0c64e448a6ca54562defd28
Jon-Dev21 <jonacevedev96@gmail.com>
2022-04-27 22:46:23 -0400

    Resubmitted.
    Removed if statement and old code comment
    
    Runtime: 247 ms, faster than 30.18% of C# online submissions for Two Sum.
    
    Memory Usage: 42.3 MB, less than 97.81% of C# online submissions for Two Sum.
    
    Somehow it ran a bit slower but it saved more memory than last submit.

--- Two Sum/Two Sum/Program.cs
+++ Two Sum/Two Sum/Program.cs
@@ -7,31 +7,13 @@
 
     public static int[] TwoSum(int[] nums, int target)
     {
-
         for (int i = 0; i < nums.Length; i++)
         {
             for(int j = i+1; j < nums.Length; j++)
             {
-                if(j < nums.Length)
-                {
-                    if (nums[i] + nums[j] == target)
-                    {
-                        return new int[] { i, j};
-                    }  
-                }
+                if (nums[i] + nums[j] == target)
+                    return new int[] { i, j};
             }
-
-
-            //if(i+1 < nums.Length) // Avoid out of bounds.
-            //{
-            //    if(nums[i] + nums[i+1] == target)
-            //    {
-            //        return new int[] { i, i + 1};
-            //    }
-            //} else if (nums[i] + nums[nums.Length-1] == target)
-            //{
-            //    return new int[] { i, nums.Length - 1 };
-            //}
         }
         return new int[] {0,0};
     }
