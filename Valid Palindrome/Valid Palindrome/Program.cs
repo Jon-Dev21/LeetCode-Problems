90ef79d30aa1258610c9b24aeb68ac5fdc8ebfc6
Jon-Dev21 <jonacevedev96@gmail.com>
2022-04-27 23:14:55 -0400

    Added the valid palindrome problem
    Half solved.
    Did not considered that Alphanumeric characters are allowed. Though it was alphabetic only. Will solve for alphanumeric characters

---
+++ Valid Palindrome/Valid Palindrome/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(IsPalindrome("A man, a plan, a canal: Panama"));
+        Console.WriteLine(IsPalindrome("race a car"));
+        Console.WriteLine(IsPalindrome("0P"));
+    }
+
+    public static bool IsPalindrome(string s)
+    {
+        string temp = "";
+        string temp2 = "";
+        s = s.ToLower().Trim();
+        for(int i = 0; i < s.Length; i++)
+        {
+            if (char.IsLetterOrDigit(s[i]))
+            {
+                temp = s[i] + temp; // Reversed trimmed and lower case string
+                temp2 += s[i]; // Original trimmed and lower case string
+            }
+        }
+        return temp == temp2;
+    }
+}
