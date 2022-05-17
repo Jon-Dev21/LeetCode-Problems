648ff76b19b1a6ec1035c4c6954e7f1a3d894cf1
Jon-Dev21 <jonacevedev96@gmail.com>
2022-05-16 21:27:02 -0400

    Found solution to Valid Palindrome
    Runtime: 1654 ms, faster than 5.79% of C# online submissions for Valid Palindrome.
    
    Memory Usage: 57.1 MB, less than 5.11% of C# online submissions for Valid Palindrome.
    
    Will attempt to optimize solution soon.

--- C#/Valid Palindrome/Valid Palindrome/Program.cs
+++ C#/Valid Palindrome/Valid Palindrome/Program.cs
@@ -7,21 +7,50 @@
         Console.WriteLine(IsPalindrome("0P"));
     }
 
+    /// <summary>
+    /// Will attempt to use stacks. 
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
     public static bool IsPalindrome(string s)
     {
+        s = s.ToLower();
+        var stringArray = s.Split(" ");
         string reversed = "";
-        string straight = "";
-        string dbReverse = "";
-        s = s.ToLower().Trim();
-        for(int i = 0; i < s.Length; i++)
+        string straight = string.Join("", stringArray);
+        int length = straight.Length-1;
+        string straightClean = "";
+
+        for (int i = length, j = 0; i >= 0 && j < length+1; i--, j++)
         {
-            if (char.IsLetterOrDigit(s[i]))
+            if (char.IsLetterOrDigit(straight[i]))
             {
-                reversed = s[i] + reversed; // Reversed trimmed and lower case string
-                straight += s[i]; // Original trimmed and lower case string
-                dbReverse += reversed;
+                reversed += straight[i];
+            } 
+            if(char.IsLetterOrDigit(straight[j]))
+            {
+                straightClean += straight[j];
             }
         }
-        return reversed == straight || dbReverse == straight;
+        return reversed == straightClean;
     }
 }
+
+    // Not fast enough.
+    //public static bool IsPalindrome(string s)
+    //{
+    //    string reversed = "";
+    //    string straight = "";
+    //    string dbReverse = "";
+    //    s = s.ToLower().Trim();
+    //    for (int i = 0; i < s.Length; i++)
+    //    {
+    //        if (char.IsLetterOrDigit(s[i]))
+    //        {
+    //            reversed = s[i] + reversed; // Reversed trimmed and lower case string
+    //            straight += s[i]; // Original trimmed and lower case string
+    //            dbReverse += reversed;
+    //        }
+    //    }
+    //    return reversed == straight || dbReverse == straight;
+    //}
